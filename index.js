d264ee511c11f46b7de182e1c409f555f8f3fc5e
Rouven Weßling <rouven@contentful.com>
2018-01-07 14:37:20 +0100

    Convert ConfigFile to an ES2015 class.

--- index.js
+++ index.js
@@ -5,39 +5,43 @@ var esprima = require('esprima');
 var util = require('util');
 var stringifyObject = require("stringify-object");
 
-exports.ConfigFile = function(filePath) {
-  var that = this;
+class ConfigFile {
+  constructor(filePath) {
+    this.filePath = filePath;
 
-  this.config = null;
+    this.config = null;
+
+    /**
+     * null = never read
+     * var = config was read with var require = {...}
+     * requirejs = config was read with requirejs.config({...})
+     * require = config was read with require.config({...})
+     * empty = no config expression was found (but read() was called)
+     * create-if-not-exists = createIfNotExists() was called so it might not have an existing file
+     */
+    this.type = null;
+
+    /**
+     * The position where the object expression should be written back to
+     */
+    this.range = null;
+
+    this.contents = null;
+  }
 
   /**
-   * null = never read
-   * var = config was read with var require = {...}
-   * requirejs = config was read with requirejs.config({...})
-   * require = config was read with require.config({...})
-   * empty = no config expression was found (but read() was called)
-   * create-if-not-exists = createIfNotExists() was called so it might not have an existing file
+   * returns the config object from the read file
    */
-  this.type = null;
-
-  /**
-   * The position where the object expression should be written back to
-   */
-  this.range = null;
-
-  this.contents = null;
-
-  // returns the config object from the read file
-  this.read = function () {
+  read() {
     var callback = function(){};
     try {
-      var data = fs.readFileSync(filePath);
+      var data = fs.readFileSync(this.filePath);
 
-      that.contents = data.toString();
+      this.contents = data.toString();
 
     } catch (err) {
-      if (err.code === 'ENOENT' && that.type === 'create-if-not-exists') {
-        return that.config;
+      if (err.code === 'ENOENT' && this.type === 'create-if-not-exists') {
+        return this.config;
       } else {
         throw err;
       }
@@ -46,49 +50,49 @@ exports.ConfigFile = function(filePath) {
     var program;
 
     try {
-      program = esprima.parse(that.contents, {range: true});
+      program = esprima.parse(this.contents, {range: true});
     } catch (ex) {
-      throw new Error('could not read: '+filePath+' because it has syntax errors: '+ex);
+      throw new Error('could not read: '+this.filePath+' because it has syntax errors: '+ex);
     }
 
-    that.type = 'empty';
+    this.type = 'empty';
     if (program.type === 'Program') {
-      program.body.forEach(function(statement) {
+      program.body.forEach(statement => {
 
         if (statement.expression && statement.expression.type === 'CallExpression') {
           var call = statement.expression;
 
           if (call.callee.type === 'MemberExpression' && (call.callee.object.name === 'requirejs' || call.callee.object.name === 'require') && call.callee.property.name === 'config') {
-            that.type = call.callee.object.name === 'require' ? 'require' : 'requirejs';
-            that.readObjectExpression(call.arguments[0], callback);
+            this.type = call.callee.object.name === 'require' ? 'require' : 'requirejs';
+            this.readObjectExpression(call.arguments[0], callback);
             return false;
           }
         } else if(statement.type === 'VariableDeclaration') {
-          statement.declarations.forEach(function(declarator) {
+          statement.declarations.forEach(declarator => {
             if (declarator.id.name === 'require') {
-              that.type = 'var';
-              that.readObjectExpression(declarator.init, callback);
+              this.type = 'var';
+              this.readObjectExpression(declarator.init, callback);
               return false;
             }
           });
 
-          if (that.type === 'var') return false;
+          if (this.type === 'var') return false;
         }
       });
     }
 
-    if (that.type === 'empty') {
-      that.config = {};
+    if (this.type === 'empty') {
+      this.config = {};
     }
 
-    return that.config;
-  };
+    return this.config;
+  }
 
-  this.write = function() {
+  write() {
     var contents;
 
     if (this.type === 'empty' || this.type === 'create-if-not-exists') {
-      contents = util.format("/* globals requirejs */\nrequirejs.config(%s);\n", that.buildConfig());
+      contents = util.format("/* globals requirejs */\nrequirejs.config(%s);\n", this.buildConfig());
 
     } else {
 
@@ -96,45 +100,47 @@ exports.ConfigFile = function(filePath) {
         throw new Error('The config cannot be written. Was it read() before? The config expression has to be found to allow writing. You can use createIfNotExists() to create an empty config.');
       }
 
-      contents = that.contents.substring(0, that.range[0]) + that.buildConfig() + that.contents.substring(that.range[1]);
+      contents = this.contents.substring(0, this.range[0]) + this.buildConfig() + this.contents.substring(this.range[1]);
     }
 
-    fs.outputFileSync(filePath, contents);
-  };
+    fs.outputFileSync(this.filePath, contents);
+  }
 
   /**
    * Creates the config(File) if not already existing
    *
    * notice: you still need to write() it to have a physical existing file.
    */
-  this.createIfNotExists = function(config) {
-    that.config = config || {};
-    that.type = 'create-if-not-exists';
-  },
+  createIfNotExists(config) {
+    this.config = config || {};
+    this.type = 'create-if-not-exists';
+  }
 
-  this.buildConfig = function() {
+  buildConfig() {
     return stringifyObject(
-      that.config,
+      this.config,
       {
         indent: '  '
       }
     );
-  };
+  }
 
-  this.readObjectExpression = function(objectExpression, callback) {
+  readObjectExpression(objectExpression, callback) {
     /* jshint evil:true */
     if (objectExpression && objectExpression.type === 'ObjectExpression') {
       try {
-        that.config = eval('('+that.contents.substring(objectExpression.range[0], objectExpression.range[1])+')');
+        this.config = eval('('+this.contents.substring(objectExpression.range[0], objectExpression.range[1])+')');
 
       } catch (syntaxError) {
         return callback(syntaxError, null);
       }
 
-      that.range = objectExpression.range;
-      return callback(null, that.config);
+      this.range = objectExpression.range;
+      return callback(null, this.config);
     }
 
     return callback('cannot read objectExpression from '+util.inspect(objectExpression));
-  };
-};
+  }
+}
+
+module.exports = {ConfigFile};
